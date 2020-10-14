0866e89822815a5b4aae9898d96886e9ffcbdf0f
Ionut Hada <hadaionut@gmail.com>
2020-10-14 10:27:05 +0300

    UI prettyfication

--- src/scripts/WallCutter.ts
+++ src/scripts/WallCutter.ts
@@ -5,27 +5,19 @@ Hooks.on("renderWallConfig", (data, html) => {
     console.log(data);
     if (!game.user.isGM) return;
 
-    let cutButton = $("<button class='import-markdown'><i class='fas fa-file-import'></i>Cut The Wall</button>");
+    let cutButton = $("<button><i class='fas fa-cut'></i>Cut The Wall</button>");
     cutButton.on('click', () => {
         WallCutting.cutTheWall(data.object);
         data.close();
     });
 
-    let mergeButton = $("<button class='import-markdown'><i class='fas fa-file-import'></i>Merge overlapping walls</button>");
+    let mergeButton = $("<button><i class='fas fa-code-branch'></i>Merge overlapping walls</button>");
     mergeButton.on('click', () => {
         WallMerger.mergeWalls(data.object);
         data.close();
     })
-    html.find(".window-content").append(cutButton);
-    html.find(".window-content").append(mergeButton);
-})
 
-Hooks.on("getSceneControlButtons", (controls) => {
-    if (!game.user.isGM) return;
-    const insertTool = {
-        name: "insertTool",
-        title: "Insert into walls",
-        icon: "fas fa-pen",
-    }
-    controls[4].tools.push(insertTool);
+    const location = html.find(".window-content").children();
+    location.append(cutButton);
+    location.append(mergeButton);
 })
